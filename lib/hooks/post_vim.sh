5a13f5fab0c45048442a37ac336a3988d7240594
Lou Gonzalez <tasogare.no.senshi@gmail.com>
2021-05-28 14:40:47 -0500

    Call nvim in hook explicitly

--- lib/hooks/post_vim.sh
+++ lib/hooks/post_vim.sh
@@ -3,10 +3,10 @@
 if [ ! -e ./vim/.vim/autoload/plug.vim ]; then
   echo 'Installing packer...'
   git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/opt/packer.nvim
-  vim -X -n +PackerInstall +qall > /dev/null
+  nvim -X -n +PackerInstall +qall > /dev/null
   echo 'Done installing vim plugins.'
 else
   echo 'Updating plugins...'
-  vim -X -n +PackerSync +PackerCompile +qall > /dev/null
+  nvim -X -n +PackerSync +PackerCompile +qall > /dev/null
   echo 'Done updating vim plugins.'
 fi
