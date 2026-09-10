434c098c6f0c104b530bb534f49ab1acf9b3fcc4
Lou Gonzalez <lou.gonzalez@proton.me>
2026-09-10 08:23:16 -0500

    feat(configs): add Helix config
    This includes a slightly more advanced version of the editor, though
    unofficial, that adds support for plugins.

---
+++ configs/.config/helix/helix.scm
@@ -0,0 +1,6 @@
+(require (prefix-in helix. "helix/commands.scm"))
+(require (prefix-in helix.static. "helix/static.scm"))
+(require "helix/editor.scm")
+(require "helix/misc.scm")
+
+(define load-buffer helix.static.load-buffer!)
